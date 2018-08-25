67022b8e0ad9f9afb2f02c32239d7204767a8c85
vselosi <romadnestr@gmai.com>
2018-08-25 13:16:26 +0300

    second commit

---
+++ test.php
@@ -0,0 +1,48 @@
+<?php
+/*$x = 0;
+ if ($x>0 ) echo $y = (sin($x))^2;
+ else echo $y = 1-2*sin(($x)^2);
+
+
+/*$x = 0;
+$z = sin(($x)^2);
+if ($x>0) echo $z;
+else echo $y = 1+2*$z;
+
+/*$x = 3;
+if ($x>4) echo "II";
+else echo "I";
+
+$y = 0;
+if ($y>3) echo "I";
+else echo "II"; */
+
+/*$x = 1;
+$y = 4;
+if ($x<$y) echo "if x = 1 & y = 4, y>x"; */
+
+/*$a = 1;
+$b = 3;
+ if ($a>$b) echo "a greater then b";
+ elseif ($b>$a) echo "b greater then a";*/
+
+/*$a = 4;
+$b = 3;
+$c = $a/$b;
+if ($a%$b==0) echo $c;
+else echo "!!!"*/
+
+/*$a = 7;
+$b = 3;
+if ($a%$b==0) echo "yes";
+else echo "no";*/
+
+/*$a = 21;
+$b = 27;
+if ($a%2==0) echo "a) yes";
+elseif ($b%10==7) echo "b) number 7 exist";*/
+
+
+
+
+?>
