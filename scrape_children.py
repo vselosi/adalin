6dce9f4e9ff8c937962b0c8338dec9dc9d98e1e9
vselosi <42691557+vselosi@users.noreply.github.com>
2018-11-07 18:17:17 +0200

    Delete scrape_children.py

--- scrape_children.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import html5lib
-
-html = urlopen(input("set the url: "))
-bsObj = BeautifulSoup(html, "html5lib")
-
-for child in bsObj.find("div",{"class":"webcam"}).children:
-    print(child)
