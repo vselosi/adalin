ca685caa086342585a7a759abc6650c6dbeb7681
vselosi <42691557+vselosi@users.noreply.github.com>
2018-11-07 09:21:51 +0200

    Add files via upload

--- scrape_cams.py
+++ scrape_cams.py
@@ -10,28 +10,24 @@ import html5lib
 random.seed(datetime.datetime.now())
 
 def getLinks(camUrl):
-
-    html = urlopen("http://webcam.scs.com.ua/europe"+camUrl)
-    bsObj = BeautifulSoup(html, "html5lib")
-    return bsObj.findAll("a", {"href":re.compile("(/ukraine/)(?!:).*$")})
+    try:
+        html = urlopen("http://webcam.scs.com.ua/europe"+camUrl)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html, "html5lib")
+    except AttributeError as e:
+        return None
+    return bsObj.find("div", {"class":"webcam"}).findAll("a", {"href":re.compile("(/ukraine/)")})
     #return bsObj.find("div", {"class":"webcam"})
 
 #links = getLinks("/ukraine/lviv/tsentr-lvova-prospekt-svobody/")
-def linksError():
-    links = getLinks("/ukraine/lviv/tsentr-lvova-prospekt-svobody/")
-    lnk = links
-    try:
-        while  len(links) > 0:
-            newCam = links[random.randint(0, len(links)-1)].attrs["href"]
-            print(newCam)
-            links = getLinks(newCam)
-    except HTTPError as e:
-        #links = getLinks("/ukraine/lviv/tsentr-lvova-prospekt-svobody/")
-        #return lnk
-#lk = linksError()
-#print(lk)
 
-        while  len(links) > 0:
-            newCam = links[random.randint(0, len(links)-1)].attrs["href"]
-            print(newCam)
-            links = getLinks(newCam)
+links = getLinks("/ukraine/lviv/tsentr-lvova-prospekt-svobody/")
+while  len(links) > 0:
+    newCam = links[random.randint(0, len(links)-1)].attrs["href"]
+if links == None:
+    print("There is something wrong!")
+else:
+    print(newCam)
+    links = getLinks(newCam)
