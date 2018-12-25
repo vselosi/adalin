1334d78f6f127c32d0580f59366cbd7c100231b8
vselosi <42691557+vselosi@users.noreply.github.com>
2018-12-25 12:53:51 +0200

    Delete scrape_parent.py

--- scrape_parent.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import html5lib
-
-html = urlopen(input("set the url: "))
-bsObj = BeautifulSoup(html, "html5lib")
-print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
